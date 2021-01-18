dfac06264d4c729fe61154bc3c7aa0160dd2de2a
xioperez01 <angie.xioperez@gmail.com>
2021-01-18 16:55:02 +0000

    Pycodestyle

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -17,7 +17,7 @@ class FileStorage():
 
     def all(self, cls=None):
         """Returns a dictionary of models currently in storage"""
-         if cls is not None:
+        if cls is not None:
             new_dict = {}
             for key, value in self.__objects.items():
                 if type(value) == cls:
@@ -59,7 +59,7 @@ class FileStorage():
             with open(FileStorage.__file_path, 'r') as f:
                 temp = json.load(f)
                 for key, val in temp.items():
-                        self.all()[key] = classes[val['__class__']](**val)
+                    self.all()[key] = classes[val['__class__']](**val)
         except FileNotFoundError:
             pass
 

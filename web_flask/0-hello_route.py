ac53aa91dd4dbc56a50174bc2c487974db9e312f
xioperez01 <angie.xioperez@gmail.com>
2021-01-19 22:59:58 +0000

    Initial code task 0

---
+++ web_flask/0-hello_route.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+Script that starts a Flask web application:
+listening on 0.0.0.0, port 5000
+Routes:
+/: display “Hello HBNB!
+"""
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello_hbnb():
+    """Display "Hello HBNB!" (text) """
+    return "Hello HBNB!"
+
+
+if __name__ == "__main":
+    app.run(host="0.0.0.0", port=5000)

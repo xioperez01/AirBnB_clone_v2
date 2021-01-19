94c82e9c0fc8166b3bf7e89cec56cdf2bfb4e1d4
xioperez01 <angie.xioperez@gmail.com>
2021-01-19 23:35:26 +0000

    Main

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -15,5 +15,5 @@ def hello_hbnb():
     return "Hello HBNB!"
 
 
-if __name__ == "__main":
-    app.run(host="0.0.0.0", port=5000)
+if __name__ == '__main__':
+    app.run(host="0.0.0.0", port=5000, debug=True)
